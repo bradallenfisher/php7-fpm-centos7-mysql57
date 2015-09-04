6bd6587f6de34787e63f6df056b90d6a878a5089
Brad Fisher <bradallenfisher@gmail.com>
2015-09-03 23:54:34 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -47,13 +47,13 @@ cat << EOF > /etc/httpd/conf.d/html.conf
 EOF
 
 # get varnish duh!
-yum install varnish -y
-sed -i 's/VARNISH_LISTEN_PORT=6081/VARNISH_LISTEN_PORT=80/g' /etc/varnish/varnish.params
-sed -i 's/Listen 80/Listen 8080/g' /etc/httpd/conf/httpd.conf
+#yum install varnish -y
+#sed -i 's/VARNISH_LISTEN_PORT=6081/VARNISH_LISTEN_PORT=80/g' /etc/varnish/varnish.params
+#sed -i 's/Listen 80/Listen 8080/g' /etc/httpd/conf/httpd.conf
 
-systemctl enable varnish
+#systemctl enable varnish
 systemctl enable httpd
-systemctl start varnish
+#systemctl start varnish
 systemctl start httpd
 
 curl -sS https://getcomposer.org/installer | php
