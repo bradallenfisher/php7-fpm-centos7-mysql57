9a4f27757b2f43a9c99c85582cf0d103f54b76c9
Brad Fisher <bradallenfisher@gmail.com>
2015-09-03 21:43:06 -0400

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
-# run as drush user.
 
-cd $HOME
-curl -sS https://getcomposer.org/installer | php
-sudo mv composer.phar /usr/local/bin/composer
-php /usr/local/bin/composer global require drush/drush:6.*
-# symlink into drush
-sudo ln -sf $HOME/.composer/vendor/drush/drush /usr/bin/drush
-# add to local path
-echo 'export PATH="$PATH:/usr/bin/drush"' >> $HOME/.bashrc
-# reload bashrc so drush calls can function
-source .bashrc
+composer global require drush/drush:7.*
+echo 'export PATH="$HOME/.composer/vendor/bin:$PATH"' >> $HOME/.bashrc
+source $HOME/.bashrc
 
 # install drush recipes
 drush dl drush_recipes -y
+drush dl drush_cleanup
+drush cc drush
+
+sudo chown -R vagrant:vagrant /var/www/
 drush dl drupal --destination=/var/www/ --drupal-project-rename=html -y
 
 cd /var/www/html
 drush site-install --db-url=mysql://root@localhost:22/test -y
-chmod -R 755 /var/www/html/sites/default/files/
-chown -R apache:apache /var/www/html/sites/default/files/
+sudo chmod -R 755 /var/www/html/sites/default/files/
+sudo chown -R apache:apache /var/www/html/sites/default/files/
+
+drush cleanup -y
+drush cook d7adminux -y
 exec bash
 
 echo "DUNZY!"
